0f25aeb39aa4a7a3d2fbebae6d3cd1c11ccfc3f3
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-09-14 19:03:59 -0300

    Backend project created

---
+++ backend/src/main/java/com/devsuperior/dscatalog/DsCatalogDevSuperiorApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dscatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsCatalogDevSuperiorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsCatalogDevSuperiorApplication.class, args);
+	}
+
+}
