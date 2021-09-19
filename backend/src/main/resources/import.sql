8e644d328d97b110611f69d2ff4033a3f45da716
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-09-19 17:18:38 -0300

    Auditing data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,3 @@
-Insert Into tb_category (name) Values ('Books');
-Insert Into tb_category (name) Values ('Electronics');
-Insert Into tb_category (name) Values ('Computers');
+Insert Into tb_category (name, created_At) Values ('Books', NOW());
+Insert Into tb_category (name, created_At) Values ('Electronics', NOW());
+Insert Into tb_category (name, created_At) Values ('Computers', NOW());
