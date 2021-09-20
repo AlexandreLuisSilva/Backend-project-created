ce45ed9f850b5401b066469e372232d60b9950e9
AlexandreLuisSilva <alexandre.luis.silva@gmail.com>
2021-09-19 21:29:20 -0300

    Product entity, SQL script

--- backend/src/main/resources/import.sql
+++
@@ -1,3 +0,0 @@
-Insert Into tb_category (name, created_At) Values ('Books', NOW());
-Insert Into tb_category (name, created_At) Values ('Electronics', NOW());
-Insert Into tb_category (name, created_At) Values ('Computers', NOW());
